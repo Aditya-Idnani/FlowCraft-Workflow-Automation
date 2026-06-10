62c601f16d119838229c8cfd7b0c37c160ec21db
Aditya Idnani <adityaidnani2@gmail.com>
2026-06-11 03:21:35 +0530

    fix nextjs16 config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
 };
 
 export default nextConfig;
